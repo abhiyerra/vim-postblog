c84a448a39a621990fc6a3f9f79d4999baba0d29
ykabhinav <ykabhinav@e7340c14-9b19-0410-9bc3-91a474ed464d>
2006-07-28 02:42:39 +0000

    Initial add of postblog.py

---
+++ postblog.py
@@ -0,0 +1,47 @@
+python << EOF
+# Post to Blog
+import re
+import vim
+import xmlrpclib
+
+blog_username = 'username'
+blog_password = 'password'
+blog_url = 'http://link.to.host/xmlrpc.php'
+
+def post_blog():
+    strid = ''
+    offsetline = 0
+
+    if vim.current.buffer[0].find('StrID:') != -1:
+        strid = vim.current.buffer[0].split(':')[1]
+        offsetline = 1 
+       
+    title = vim.current.buffer[offsetline + 0]
+    tags = vim.current.buffer[offsetline + 1]
+    text = '\n'.join(vim.current.buffer[offsetline + 2:])
+    tags = '[tags]' + tags + '[/tags]\n'
+
+    content = tags + text
+
+    wp = xmlrpclib.ServerProxy(blog_url)
+    post = { 
+        'title': title,
+        'description': content 
+    }
+
+    if strid == '':
+        strid = wp.metaWeblog.newPost('', blog_username, 
+            blog_password, post, 1)
+
+        vim.current.buffer.append('\n')
+        vim.current.buffer[:] = ['StrID:' + strid] + \
+            [i for i in vim.current.buffer[:]]
+    else:
+        wp.metaWeblog.editPost(strid, blog_username,
+            blog_password, post, 1)
+
+    vim.command('set nomodified') 
+
+def get_post():
+    pass
+EOF
